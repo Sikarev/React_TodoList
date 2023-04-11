1ed6e4ef445354b74bf48761397773907d154076
Sikarev <roman.sikarev@kepler.team>
2023-04-11 16:32:13 +0300

    Todo list implementation

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import './styles.css';
+import {useEffect, useState} from "react";
+import {NewTodoForm} from "./NewTodoForm.jsx";
+import {TodoList} from "./TodoList.jsx";
+
+function App() {
+    const LOCAL_STORAGE_TODOS_PATH = 'TODOS';
+
+    const [todos, setTodos] = useState(() => {
+        const localValue = localStorage.getItem(LOCAL_STORAGE_TODOS_PATH);
+        if (!localValue) {
+            return [];
+        }
+        return JSON.parse(localValue);
+    });
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_TODOS_PATH, JSON.stringify(todos));
+    }, [todos]);
+
+    function addTodo(title) {
+        setTodos((currentTodos) => [
+            ...currentTodos, {
+                id: crypto.randomUUID(),
+                title,
+                completed: false,
+            },
+        ]);
+    }
+
+    function toggleTodo(id, completed) {
+        setTodos((currentTodos) => {
+            return currentTodos.map(todo => {
+                if (todo.id === id) {
+                    return { ...todo, completed };
+                }
+                return todo;
+            })
+        });
+    }
+
+    function deleteTodo(id) {
+        setTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id !== id);
+        })
+    }
+
+    return (
+        <>
+            <NewTodoForm onSubmit={addTodo}/>
+            <h1 className='header'>Todo List</h1>
+            <TodoList todos={todos}
+                      toggleTodo={toggleTodo}
+                      deleteTodo={deleteTodo}/>
+        </>
+    )
+}
+
+export default App
